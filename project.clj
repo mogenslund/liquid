e19b1db936e302b5593743321cb2e3af3ad9f0c9
Mogens Brødsgaard Lund <molu@localhost.localdomain>
2016-11-06 19:20:25 +0100

    Modified project to match release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject liq "0.4.0" ;"0.5.0-SNAPSHOT"
+(defproject liq "0.4.1" ;"0.4.2-SNAPSHOT"
   :description "This project is an attempt to create a text editor for editing
                Clojure files and Markdown files. It operates primarily in a
                terminal, but with dynamics and extensibilities inspired by
