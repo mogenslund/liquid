ababc889feaae62e93f9f9c3a5d1711ac49af2ad
Mogens Brødsgaard Lund <salza@salza.dk>
2017-05-08 20:39:57 +0200

    TTY prints directly to tty to prevent interference from println.

--- src/dk/salza/liq/adapters/tty.clj
+++ src/dk/salza/liq/adapters/tty.clj
@@ -7,11 +7,23 @@
 
 (def old-lines (atom {}))
 (def updater (ref (future nil)))
+(def sysout (System/out))
+
+(defn tty-print
+  [arg]
+  ;(print arg))
+  (.print sysout arg))
+
+(defn tty-println
+  [arg]
+  ;(println arg))
+  (.println sysout arg))
 
 (defn reset
   []
   (reset! old-lines {})
-  (print "\033[0;37m\033[2J"))
+  ;(tty-print "\033[0;37m\033[2J")
+  )
 
 (defn rows
   []
@@ -19,7 +31,7 @@
     (if (or (re-find #"^\d+" shellinfo) (> n 10)) 
       (Integer/parseInt (re-find #"^\d+" shellinfo))
       (do
-        (println n)
+        (tty-println n)
         (Thread/sleep 100)
         (recur (with-out-str (util/cmd "/bin/sh" "-c" "stty size </dev/tty")) (inc n))))))
 
@@ -29,14 +41,14 @@
     (if (or (re-find #"\d+$" shellinfo) (> n 10)) 
       (Integer/parseInt (re-find #"\d+$" shellinfo))
       (do
-        (println n)
+        (tty-println n)
         (Thread/sleep 100)
         (recur (with-out-str (util/cmd "/bin/sh" "-c" "stty size </dev/tty")) (inc n))))))
 
 (defn print-color
   [index & strings] ;   0         1          2        3          4         5        6    7   8        9     10
   (let [colorpalette ["0;37" "38;5;131" "38;5;105" "38;5;11" "38;5;40" "38;5;117" "42" "44" "45" "48;5;235" "49"]]
-    (print (str "\033[" (colorpalette index) "m" (apply str strings)))))
+    (tty-print (str "\033[" (colorpalette index) "m" (apply str strings)))))
 
 (defn print-lines
   [lineslist]
@@ -44,7 +56,7 @@
   ;; Redraw whole screen once in a while
   ;; (when (= (rand-int 100) 0)
   ;;  (reset! old-lines {})
-  ;;  (print "\033[0;37m\033[2J"))
+  ;;  (tty-print "\033[0;37m\033[2J"))
   (doseq [line (apply concat lineslist)]
     (let [row (line :row)
           column (line :column)
@@ -55,13 +67,13 @@
       (let [diff (max 1 (- (count (filter #(and (string? %) (not= % "")) oldcontent))
                            (count (filter #(and (string? %) (not= % "")) content))))
             padding (format (str "%" diff "s") " ")]
-        (print (str "\033[" row ";" column "H\033[s"))
+        (tty-print (str "\033[" row ";" column "H\033[s"))
         (print-color  9 " ")
         (print-color 0)
         (print-color 10)
         (doseq [ch (line :line)]
           (if (string? ch)
-            (if (= ch "\t") (print (char 172)) (print ch)) 
+            (if (= ch "\t") (tty-print (char 172)) (tty-print ch)) 
             (do
               (cond (= (ch :face) :string) (print-color 1)
                     (= (ch :face) :comment) (print-color 2)
@@ -77,7 +89,7 @@
             )))
         (if (= row (count (first lineslist)))
           (do
-            (print (str "  " padding))
+            (tty-print (str "  " padding))
             (print-color 0))
           (print-color 10 padding)))
       (swap! old-lines assoc key content))
@@ -87,6 +99,7 @@
 
 (defn view-draw
   []
+  (when (empty? @old-lines) (tty-print "\033[0;37m\033[2J"))
   (print-lines (renderer/render-screen)))
 
 (defn view-handler
@@ -120,7 +133,7 @@
 (defn view-init
   []
   (util/cmd "/bin/sh" "-c" "stty -echo raw </dev/tty")
-  (print "\033[0;37m\033[2J")
-  (print "\033[?25l") ; Hide cursor
-  (print "\033[?7l") ; disable line wrap
+  (tty-print "\033[0;37m\033[2J")
+  (tty-print "\033[?25l") ; Hide cursor
+  (tty-print "\033[?7l") ; disable line wrap
   (add-watch editor/updates "tty" view-handler))
