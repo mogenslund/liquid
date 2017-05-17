ac4a56b54ce44e17c1ebca15355d608a5f559cd1
Mogens Brødsgaard Lund <salza@salza.dk>
2017-05-17 17:49:32 +0200

    Added test to cover case where wrapping effects calculation of top of window.

--- test/dk/salza/liq/renderer_test.clj
+++ test/dk/salza/liq/renderer_test.clj
@@ -12,16 +12,17 @@
   ;  (println l))
   (clojure.string/join (filter #(not (map? %)) ((first (first (render-screen))) :line))))
 
+;; To simulate editor size: lein run --jframe --no-init-file --rows=7
 (defn reset-editor
   "Resets the editor and inserts an empty buffer with 6 real lines"
   []
   (editor/reset)
-  (editor/add-window (window/create "win1" 1 1 6 40 "buffer1"))
+  (editor/add-window (window/create "win1" 1 1 6 20 "buffer1"))
   (editor/new-buffer "buffer1"))
 
 ;; lein test :only dk.salza.liq.renderer-test/basic-renderer-test
 (deftest basic-renderer-test
-  (testing "Creating editor and simple buffer insert"
+  (testing "Calculation of top of window. Basic case"
     (reset-editor)
     (editor/insert "1abc\n2abc\n3abc\n4abc\n5abc\n6abc\n7abc")
     (editor/beginning-of-buffer)
@@ -30,4 +31,16 @@
     (is (= (get-first-line) "1abc"))
     (editor/forward-line)
     (is (= (get-first-line) "6abc"))
+    ))
+
+(deftest wrapped-line-renderer-test
+  (testing "Calculation of top of window. Case where a line is wrapping"
+    (reset-editor)
+    (editor/insert "11abc\n22abc 22222 22222 22222 22222\n33abc\n44abc\n55abc\n66abc\n77abc")
+    (editor/beginning-of-buffer)
+    (is (= (get-first-line) "11abc"))
+    (dotimes [n 5] (editor/forward-line))
+    (is (= (get-first-line) "11abc"))
+    (editor/forward-line)
+    (is (= (get-first-line) "55abc"))
     ))
