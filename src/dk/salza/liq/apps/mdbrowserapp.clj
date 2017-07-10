59394b4a29277606af5ffba993b21714f882f6b5
Mogens Brødsgaard Lund <salza@salza.dk>
2017-07-10 09:38:52 +0200

    Implemented basic url navigation in mdbrowserapp.

--- src/dk/salza/liq/apps/mdbrowserapp.clj
+++ src/dk/salza/liq/apps/mdbrowserapp.clj
@@ -1,5 +1,6 @@
 (ns dk.salza.liq.apps.mdbrowserapp
-  (:require [dk.salza.liq.editor :as editor]
+  (:require [clojure.java.io :as io]
+            [dk.salza.liq.editor :as editor]
             [dk.salza.liq.editoractions :as editoractions]
             [dk.salza.liq.keys :as keys]
             [dk.salza.liq.apps.promptapp :as promptapp]
@@ -8,10 +9,13 @@
             [dk.salza.liq.syntaxhl.clojuremdhl :as clojuremdhl]
             [dk.salza.liq.coreutil :refer :all]))
 
+(def navigate (atom nil))
+
 (def keymap
   {:cursor-color :blue
    :M editoractions/prompt-to-tmp
-   :enter editor/context-action
+   :enter #(@navigate)
+   :tab #(editor/find-next "[")
    :space #(editor/forward-page)
    :right editor/forward-char
    :left editor/backward-char
@@ -49,7 +53,19 @@
    :C-t (fn [] (editor/tmp-test))
    })
 
+;; Links:
+;; [direct.md]
+;;
+;; [My Link][1]
+;; [1]: Path to link
+
+;; :enter should open link in mdbrowserapp
+;; :tab Move cursor to next link
 (defn run
+  "TODO: Implementation not finished yet.
+  App to browse files written in markdown.
+  TODO: Support browsing webpages by converting to
+  markdown."
   [filepath]
   (if (editor/get-buffer filepath)
     (editor/switch-to-buffer filepath)
@@ -57,3 +73,15 @@
       (editor/create-buffer-from-file filepath)
       (editor/set-keymap keymap)
       (editor/set-highlighter clojuremdhl/next-face))))
+
+(reset! navigate
+  (fn []
+    (let [context (editor/get-context)
+          type (context :type)
+          value (context :value)]
+      (cond (= type :file) (if (.isAbsolute (io/file value))
+                             (run value)
+                             (run (str (io/file (editor/get-folder) value))))
+            (= type :url) (when-let [start-browser (editor/setting :start-browser)]
+                            (start-browser value))
+            (= type :function) (editor/goto-definition value)))))
