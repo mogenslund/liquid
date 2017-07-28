b51933ee38195d30e8e2fd767dc4861297db6065
Mogens Lund <salza@salza.dk>
2017-07-28 17:17:48 +0200

    Removed unused references.

--- src/dk/salza/liq/apps/mdbrowserapp.clj
+++ src/dk/salza/liq/apps/mdbrowserapp.clj
@@ -3,7 +3,6 @@
             [dk.salza.liq.buffer :as buffer]
             [dk.salza.liq.slider :refer :all]
             [dk.salza.liq.editor :as editor]
-            [dk.salza.liq.editoractions :as editoractions]
             [dk.salza.liq.keys :as keys]
             [dk.salza.liq.apps.promptapp :as promptapp]
             [dk.salza.liq.extensions.headlinenavigator]
@@ -16,7 +15,7 @@
 
 (def keymap
   {:cursor-color :blue
-   :M editoractions/prompt-to-tmp
+   :M editor/prompt-to-tmp
    :enter #(@navigate)
    :tab #(editor/find-next "[")
    :space #(editor/forward-page)
@@ -66,10 +65,10 @@
         (let [sl (state :sl)
               st (state :state)
               c (get-char sl)]
-          (cond (= st 1) (cond (or (= c "\n") (beginning? sl)) ((slider/get-context sl0) :value)
+          (cond (= st 1) (cond (or (= c "\n") (beginning? sl)) ((get-context sl0) :value)
                                (= c "[") (recur (assoc state :state 2 :sl (right sl 1)))
                                true (recur (assoc state :sl (left sl 1))))
-                (= st 2) (cond (or (= c "\n") (end? sl)) ((slider/get-context sl0) :value)
+                (= st 2) (cond (or (= c "\n") (end? sl)) ((get-context sl0) :value)
                                (= c "]") (recur (assoc state :state 3 :sl (right sl 1)))
                                true (recur (assoc state :link1 (str (state :link1) c) :sl (right sl 1))))
                 (= st 3) (cond (= c "(") (recur (assoc state :state 4 :sl (right sl 1)))
