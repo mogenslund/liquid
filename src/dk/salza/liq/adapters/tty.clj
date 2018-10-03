4de6ba9cf5337573d8f3fd720105a92acd852bcc
Mogens Brødsgaard Lund <salza@salza.dk>
2018-10-03 09:32:19 +0200

    Fixed issue with escape code sequences during fast repeat.

--- src/dk/salza/liq/adapters/tty.clj
+++ src/dk/salza/liq/adapters/tty.clj
@@ -123,6 +123,7 @@
 ;; http://ascii-table.com/ansi-escape-sequences.php
 (defn- raw2keyword
   [raw]
+  ;(logging/log "RAW" (pr-str raw)) 
   (if (integer? raw)
     (cond (= raw 127) "backspace"
           (>= raw 32) (str (char raw))
@@ -131,33 +132,34 @@
           (<= 1 raw 26) (str "C-" (char (+ raw 96)))
           (= raw 0) "C- "
           true (str (char raw)))
-    (let [c0 (first raw)
+    (let [raw2 (conj (take-while #(not= % 27) (rest raw)) 27)
+          c0 (first raw)
           c1 (second raw)
-          n (count raw)]
+          n (count raw2)]
       (cond (and (= n 1) (= c0 27)) "esc"
             (and (= n 2) (>= c1 32)) (str "M-" (char c1))
             (and (= n 2) (= c1 13)) "M-\n"
             (and (= n 2) (= c0 27) (<= 1 c1 26)) (str "C-M-" (char (+ c1 96)))
             (and (= n 2) (= c0 27)) (str "M-" (char c1))
-            (= raw '(27 91 65)) "up"
-            (= raw '(27 91 66)) "down"
-            (= raw '(27 91 67)) "right"
-            (= raw '(27 91 68)) "left"
-            (= raw '(27 91 72)) "home"
-            (= raw '(27 91 70)) "end"
-            (= raw '(27 91 53 126)) "pgup"
-            (= raw '(27 91 54 126)) "pgdn"
-            (= raw '(27 91 50 126)) "ins"
-            (= raw '(27 91 51 126)) "del"
-            (= raw '(27 79 81)) "f2"
-            (= raw '(27 79 82)) "f3"
-            (= raw '(27 79 83)) "f4"
-            (= raw '(27 91 49 53 126)) "f5"
-            (= raw '(27 91 49 55 126)) "f6"
-            (= raw '(27 91 49 56 126)) "f7"
-            (= raw '(27 91 49 57 126)) "f8"
-            (= raw '(27 91 50 48 126)) "f9"
-            (= raw '(27 91 50 52 126)) "f12"
+            (= raw2 '(27 91 65)) "up"
+            (= raw2 '(27 91 66)) "down"
+            (= raw2 '(27 91 67)) "right"
+            (= raw2 '(27 91 68)) "left"
+            (= raw2 '(27 91 72)) "home"
+            (= raw2 '(27 91 70)) "end"
+            (= raw2 '(27 91 53 126)) "pgup"
+            (= raw2 '(27 91 54 126)) "pgdn"
+            (= raw2 '(27 91 50 126)) "ins"
+            (= raw2 '(27 91 51 126)) "del"
+            (= raw2 '(27 79 81)) "f2"
+            (= raw2 '(27 79 82)) "f3"
+            (= raw2 '(27 79 83)) "f4"
+            (= raw2 '(27 91 49 53 126)) "f5"
+            (= raw2 '(27 91 49 55 126)) "f6"
+            (= raw2 '(27 91 49 56 126)) "f7"
+            (= raw2 '(27 91 49 57 126)) "f8"
+            (= raw2 '(27 91 50 48 126)) "f9"
+            (= raw2 '(27 91 50 52 126)) "f12"
             true (str (char c0))))))
 
 (defn input-handler
